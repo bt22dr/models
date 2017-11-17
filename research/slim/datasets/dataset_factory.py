6480b367c05758fd20f56e1a51104f364bb317c0
jy_song <jy_song@tmon.co.kr>
2017-11-17 11:29:00 +0900

    add data converter for cdiscount_0_2K

--- research/slim/datasets/dataset_factory.py
+++ research/slim/datasets/dataset_factory.py
@@ -22,12 +22,14 @@ from datasets import cifar10
 from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
+from datasets import cdiscount_0_2K
 
 datasets_map = {
     'cifar10': cifar10,
     'flowers': flowers,
     'imagenet': imagenet,
     'mnist': mnist,
+    'cdiscount_0_2K': cdiscount_0_2K,
 }
 
 
